e5ce638c0a4522f1bd2589470ecb2e854a3ccb5c
ciceroinfo <cicero.info@gmail.com>
2019-11-02 23:51:44 -0300

    Implementação do binding do componente

--- src/main/java/br/com/cursojsf/beans/ExemploViewScoped.java
+++ src/main/java/br/com/cursojsf/beans/ExemploViewScoped.java
@@ -2,20 +2,25 @@ package br.com.cursojsf.beans;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ViewScoped
 @ManagedBean
 public class ExemploViewScoped {
 	
-	private static final Logger logger = Logger.getLogger(ExemploViewScoped.class.getName());
+	private static final Logger logger = LoggerFactory.getLogger(ExemploViewScoped.class.getName());
 
 	private String nome;
 	private List<String> nomes;
+	
+	private HtmlCommandButton bindingCommandButton;
 
 	@PostConstruct
 	public void setup() {
@@ -27,6 +32,10 @@ public class ExemploViewScoped {
 		logger.info("Nome ".concat(nome));
 
 		nomes.add(nome);
+		
+		if (nomes.size() > 3) {
+			bindingCommandButton.setDisabled(true);
+		}
 
 		/* retorno vazio vai para a mesma pagina */
 		return "";
@@ -46,6 +55,14 @@ public class ExemploViewScoped {
 
 	public void setNomes(List<String> nomes) {
 		this.nomes = nomes;
+	}
+
+	public HtmlCommandButton getBindingCommandButton() {
+		return bindingCommandButton;
+	}
+
+	public void setBindingCommandButton(HtmlCommandButton bindingCommandButton) {
+		this.bindingCommandButton = bindingCommandButton;
 	}
 
 }
